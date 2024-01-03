cf3374442abe9945df276c38c66f1c64cc505844
Jescel James <jesceljames@gmail.com>
2024-01-02 23:30:34 -0500

    moved db to db folder

--- db/schema.sql
+++ db/schema.sql
@@ -13,8 +13,8 @@ CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INT
-  -- FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
@@ -22,9 +22,9 @@ CREATE TABLE employees (
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
-  manager_id INT
---   -- FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
---   -- FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+  FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
 
 
