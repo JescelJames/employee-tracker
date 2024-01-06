87f099c90fe80590dab1343f957c8337b8f102a0
Jescel James <jesceljames@gmail.com>
2024-01-06 17:21:48 -0500

    deleted comments

--- Develop/index.js
+++ Develop/index.js
@@ -5,25 +5,6 @@
     const db = require('./config/connection');
     require('console.table'); // to printout that looks like mysql
 
-// // CONNECT TO DATABASE ___________________________________    
-
-//     // Connect to database
-//     const db = mysql.createConnection(
-//         {
-//             host: 'localhost',
-//             user: 'root',
-//             password: 'rootroot',
-//             database: 'employee_tracker_db',
-            
-//         },
-//         console.log(`Connected to the employee_tracker_db database.`)
-//         );
-
-        
-//         // db.query('SELECT * FROM departments', function (err, results) {
-//         //     console.log(results);
-//         //     });
-
 
 // MAIN PROMPT QUESTIONS __________________________________
     const questions = [
