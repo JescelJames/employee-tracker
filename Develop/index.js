06a7d09e682e30eb9c9851209e64587e281aeddf
Jescel James <jesceljames@gmail.com>
2024-01-07 09:52:15 -0500

    view employees modified to capture correct data

--- Develop/index.js
+++ Develop/index.js
@@ -92,7 +92,19 @@
     // View All Employees Function -----------------------
 
         function viewAllEmployees() {
-            const query = `SELECT * FROM employees`;
+            const query = `SELECT 
+            e.id AS 'Employee ID', 
+            e.first_name AS 'First Name', 
+            e.last_name AS 'Last Name', 
+            r.title AS 'Job Title', 
+            d.name AS 'Department', 
+            r.salary AS 'Salary',
+            CONCAT(m.first_name, ' ', m.last_name) AS 'Manager'
+        FROM employees e
+        JOIN roles r ON e.role_id = r.id
+        JOIN departments d ON r.department_id = d.id
+        LEFT JOIN employees m ON e.manager_id = m.id;
+        `;
             db.query(query, function (err, results) {
                 if (err) {
                     console.error('Error occurred:', err);
